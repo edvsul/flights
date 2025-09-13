52e1bde854a09cf7e2b791f2f2a981fb274a8c21
esulzickis <esulzickis@toogoodtogo.com>
2025-09-13 19:56:20 +0200

    feat: add Selenium dependencies and imports for web scraping functionality

--- fetch_flight_price_selenium.py
+++ fetch_flight_price_selenium.py
@@ -10,6 +10,15 @@ import logging
 import re
 import json
 
+# Add Selenium imports
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from selenium.common.exceptions import TimeoutException, NoSuchElementException, WebDriverException
+
 # Set up logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
